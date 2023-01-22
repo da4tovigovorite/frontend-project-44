2e05a33f2ef51d3c06a626d74eac6b7a9de1187c
pnk13 <da4tovigovorite@gmail.com>
2023-01-22 17:29:14 +0300

    fix according eslint

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,39 +1,36 @@
-import gameLogic from '../index.js';
 import _ from 'lodash';
+import { gameLogic } from '../index.js';
 
 const rules = `What is the result of the expression?`;
 
 const arrQuestionAndCorrectAnswer = () => {
-    const randomFirstNumber = _.random(1, 10);
-    const randomSecondNumber = _.random(1, 10);
+  const randomFirstNumber = _.random(1, 10);
+  const randomSecondNumber = _.random(1, 10);
 
-    const mathematicalOperations = [42,43,45]; // Ascii: 42 is "*" ; 43 is "+" ; 45  is "-" 
-    const randomMathOp = mathematicalOperations[Math.floor(Math.random()*mathematicalOperations.length)]; // random between [42,43,45]
-    const arithmeticOperation = String.fromCharCode(randomMathOp); // convert Ascii symbol to a char
+  const mathematicalOperations = [42, 43, 45]; // Ascii: 42 is "*" ; 43 is "+" ; 45  is "-"
+  // random between [42,43,45]
+  const randomMathOp = mathematicalOperations[
+    Math.floor(Math.random() * mathematicalOperations.length)];
+  const arithmeticOperation = String.fromCharCode(randomMathOp); // convert Ascii symbol to a char
 
-    let gameResult = 0;
-    // which of the operations ( + , - , * ) will be performed
-    if (arithmeticOperation == '+') {
-        gameResult = randomFirstNumber + randomSecondNumber;
-    }   else if (arithmeticOperation == '-') {
-        gameResult = randomFirstNumber - randomSecondNumber;
-        }
-        else gameResult = randomFirstNumber * randomSecondNumber;
-    
-    const gameQuestion = `${randomFirstNumber} ${arithmeticOperation} ${randomSecondNumber}`;
+  let gameResult = 0;
+  // which of the operations ( + , - , * ) will be performed
+  if (arithmeticOperation === `+`) {
+    gameResult = randomFirstNumber + randomSecondNumber;
+  } else if (arithmeticOperation === `-`) {
+    gameResult = randomFirstNumber - randomSecondNumber;
+  } else gameResult = randomFirstNumber * randomSecondNumber;
 
-    return [gameQuestion, gameResult]; 
+  const gameQuestion = `${randomFirstNumber} ${arithmeticOperation} ${randomSecondNumber}`;
+
+  return [gameQuestion, gameResult];
 };
-
 
 const gameCalc = () => {
-    const arrOutput = [];
-    for (let i = 0; i < 3; i += 1) {
-        arrOutput.push(arrQuestionAndCorrectAnswer());
-    }
-    gameLogic (rules, arrOutput)
+  const arrOutput = [];
+  for (let i = 0; i < 3; i += 1) {
+    arrOutput.push(arrQuestionAndCorrectAnswer());
+  }
+  gameLogic(rules, arrOutput);
 };
 export default gameCalc;
-
-
-
--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,20 @@
-import gameLogic from '../index.js';
 import _ from 'lodash';
+import { gameLogic } from '../index.js';
 
 const rules = `Answer "yes" if the number is even, otherwise answer "no".`;
 
-const isEven = (num) => (num % 2 == 0 ? 'yes' : 'no'); // isEven is a string
-
-
+const isEven = (num) => (num % 2 === 0 ? `yes` : `no`); // isEven is a string
 
 const arrQuestionAndCorrectAnswer = () => {
-    const randomNumber = _.random(1, 10);
-    return [randomNumber, isEven(randomNumber)]; 
-}
+  const randomNumber = _.random(1, 10);
+  return [randomNumber, isEven(randomNumber)];
+};
 
 const gameEven = () => {
-    const arrOutput = [];
-    for (let i = 0; i < 3; i += 1) {
-        arrOutput.push(arrQuestionAndCorrectAnswer());
-    }
-    gameLogic (rules, arrOutput)
+  const arrOutput = [];
+  for (let i = 0; i < 3; i += 1) {
+    arrOutput.push(arrQuestionAndCorrectAnswer());
+  }
+  gameLogic(rules, arrOutput);
 };
 export default gameEven;
